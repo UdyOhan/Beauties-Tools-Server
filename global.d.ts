dbe397a41a93d29583fe2963d1625d271260e1b5
UdyOhan <93688289+UdyOhan@users.noreply.github.com>
2024-07-07 23:48:31 +0100

    Remove global.d.ts from gitignore

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string = "8080";
+      MONGO_URI: string = "mongodb+srv://chibudomohanwe:I9ZhfKzg9OTUrL1W@cluster0.v2ancpg.mongodb.net/beauties_tools?retryWrites=true&w=majority&appName=Cluster0";
+    }
+}
